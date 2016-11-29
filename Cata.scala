c96ca88d30bab9f18d86e0960aa4ff4d2a2bbb93
Hamish Dickson <hamish.dickson@gmail.com>
2016-11-29 20:58:40 +0000

    implement filter

--- Cata.scala
+++ Cata.scala
@@ -3,16 +3,14 @@ package cata
 /**
   * Catamorphism
   *
-  * Implementation over a list
+  * This implementation is over a list
   */
-
-object Cata {
+trait Cata {
   def cata[A, B](ls: List[A], b: B, f: (A, B) => B): B = ls match {
     case Nil => b
     case a :: as => f(a, cata(as, b, f))
   }
 
   def length[A](ls: List[A]): Int = cata(ls, 0, (_: A, b: Int) => 1 + b)
-  def filter[A](ls: List[A]): List[A] = ???
-
+  def filter[A](ls: List[A])(p: A => Boolean): List[A] = cata(ls, Nil, (a: A, b: List[A]) => if(p(a)) a :: b else b)
 }
