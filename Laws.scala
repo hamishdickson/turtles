5aca910bfad907bdcd9b4ef48e9652f36cb90da8
Hamish Dickson <hamish.dickson@gmail.com>
2016-12-06 22:21:04 +0000

    add stuff for scalatest

---
+++ Laws.scala
@@ -0,0 +1,12 @@
+package laws
+
+import org.scalacheck.Prop
+import org.scalatest.{Matchers, FlatSpec}
+
+class CataTest extends FlatSpec with Matchers {
+  "A cata" should "obey the fusion law" in {
+    /*
+     * f . banana(b, |+|) = banana(c, |x|) <= f b = c ^ f (a |+| as) = a |x| (f as)
+     */
+  }
+}
--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.12.0"
 
 libraryDependencies ++= Seq(
   "org.typelevel" %% "cats" % "0.8.1",
-  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
+  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
+  "org.scalatest" %% "scalatest" % "3.0.1"
 )
